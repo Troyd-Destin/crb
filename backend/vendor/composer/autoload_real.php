c20f94bb337d637f874db5ed04abe08101a5a88b
Tyson Boyce <Tyson Boyce>
2017-06-01 13:49:55 -0600

    2017-06-01 Vendor Update

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitde10f2a7c8e308ea36f86a0c2aeec678
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitde10f2a7c8e308ea36f86a0c2aeec678', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
